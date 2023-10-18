cacb58616fe88b7a29100d6336646b67bdeafb6a
E_Krantz <eliaskra@kth.se>
2023-10-18 11:24:43 +0200

    Update startup_px4_comm.sh to use micro-ros

--- startup/startup_px4_comm.sh
+++ startup/startup_px4_comm.sh
@@ -8,7 +8,8 @@ fi
 # Source ROS2 workspaces
 source /opt/ros/foxy/setup.bash
 source /home/discower/PX4-Space-Systems_ROS2_WS/install/setup.bash
-
+source /home/discower/microros_ws/install/local_setup.bash
 
 # Run MicroXRCEAgent
-MicroXRCEAgent udp4 -p 8888 > /dev/null 2>&1 &
+# MicroXRCEAgent udp4 -p 8888 > /dev/null 2>&1 &
+ros2 run micro_ros_agent micro_ros_agent udp4 -p 8888 > /dev/null 2>&1 &
